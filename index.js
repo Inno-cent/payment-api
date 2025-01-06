3c26f45671d2fd5829906c69db165df5993c5c1b
inno-cent <ekweozorinnocent@gmail.com>
2025-01-06 15:50:18 +0100

    feat : quick app setup

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = express()
 
 var Client = require('coinbase').Client;
-var client = new Client({'apiKey': mykey, 'apiSecret': mysecret});
+var client = new Client({'apiKey': process.env.COINBASE_API_KEY, 'apiSecret':  process.env.COINBASE_API_SECRET});
 
 app.listen(8000, () => {
 console.log("server is running on PORT 8000")
